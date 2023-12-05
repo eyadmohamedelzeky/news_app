b0c3dbcfbeb64bbfc4bea0964123e316fb946bd3
eyad <eyadmohamedelzeky@gmail.com>
2023-12-05 21:37:08 +0200

    add firebase to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:new_app/app/app.dart';
 import 'package:new_app/app/bloc_observer.dart';
 import 'package:new_app/core/di/di.dart';
+import 'package:new_app/firebase_options.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+ // WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = MyBlocObserver();
   await configureDependencies();
   runApp(const MyApp());
