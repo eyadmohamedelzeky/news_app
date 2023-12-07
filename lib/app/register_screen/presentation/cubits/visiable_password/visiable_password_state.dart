3274b8fd03be5c119780c9b5d75213991af21e7f
eyad <eyadmohamedelzeky@gmail.com>
2023-12-07 10:41:30 +0200

    finished visiable password statemanagement

---
+++ lib/app/register_screen/presentation/cubits/visiable_password/visiable_password_state.dart
@@ -0,0 +1,8 @@
+part of 'visiable_password_cubit.dart';
+
+abstract class VisiablePasswordState {
+  const VisiablePasswordState();
+}
+class VisiablepasswordInitial extends VisiablePasswordState {}
+class VisiablepasswordSuccess extends VisiablePasswordState {}
+class VisiablepasswordFailure extends VisiablePasswordState {}
