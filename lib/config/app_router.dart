60de0d67a2fd10b2b6418ad02a10c0f6d684bc76
eyad <eyadmohamedelzeky@gmail.com>
2023-12-07 10:42:33 +0200

    add routes

--- lib/config/app_router.dart
+++ lib/config/app_router.dart
@@ -1,7 +1,10 @@
 import 'package:go_router/go_router.dart';
 import 'package:new_app/app/home_screen/presentation/screens/home_screen.dart';
+import 'package:new_app/app/login_screen/presentation/screens/login_screen.dart';
+import 'package:new_app/app/register_screen/presentation/screens/register_screen.dart';
 import 'package:new_app/app/splash_screen/presentation/screens/splash_screen.dart';
 import 'package:new_app/config/routes_names.dart';
+
 final router = GoRouter(
   initialLocation: AppRouteName.splashScreen,
   debugLogDiagnostics: true,
@@ -15,6 +18,16 @@ final router = GoRouter(
       path: AppRouteName.homeScreen,
       name: AppRouteName.homeScreen,
       builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: AppRouteName.loginScreen,
+      name: AppRouteName.loginScreen,
+      builder: (context, state) => const LoginScreen(),
+    ),
+    GoRoute(
+      path: AppRouteName.registerScreen,
+      name: AppRouteName.registerScreen,
+      builder: (context, state) => const RegisterScreen(),
     ),
   ],
 );
--- lib/config/routes_names.dart
+++ lib/config/routes_names.dart
@@ -1,4 +1,6 @@
 class AppRouteName {
   static const String splashScreen = "/";
   static const String homeScreen = "/HomeScreen";
+  static const String loginScreen = "/LoginScreen";
+  static const String registerScreen = "/RegisterScreen";
 }
